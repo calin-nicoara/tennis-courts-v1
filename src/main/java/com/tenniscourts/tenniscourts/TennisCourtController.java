6e83f77c5be87f336692d4508532fd7c95bc453a
Calin Nicoara <c.i.nicoara@gmail.com>
2022-02-09 23:15:22 +0200

    Added Rest annotations to TennisCourtController

--- src/main/java/com/tenniscourts/tenniscourts/TennisCourtController.java
+++ src/main/java/com/tenniscourts/tenniscourts/TennisCourtController.java
@@ -1,25 +1,34 @@
 package com.tenniscourts.tenniscourts;
 
 import com.tenniscourts.config.BaseRestController;
-import lombok.AllArgsConstructor;
-import org.springframework.http.ResponseEntity;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/tennis-courts")
 @AllArgsConstructor
 public class TennisCourtController extends BaseRestController {
 
     private final TennisCourtService tennisCourtService;
 
-    //TODO: implement rest and swagger
+    @PostMapping
     public ResponseEntity<Void> addTennisCourt(TennisCourtDTO tennisCourtDTO) {
         return ResponseEntity.created(locationByEntity(tennisCourtService.addTennisCourt(tennisCourtDTO).getId())).build();
     }
 
-    //TODO: implement rest and swagger
-    public ResponseEntity<TennisCourtDTO> findTennisCourtById(Long tennisCourtId) {
+    @GetMapping("/{id}")
+    public ResponseEntity<TennisCourtDTO> findTennisCourtById(@PathVariable("id") Long tennisCourtId) {
         return ResponseEntity.ok(tennisCourtService.findTennisCourtById(tennisCourtId));
     }
 
-    //TODO: implement rest and swagger
+    @GetMapping("/scheduled/{id}")
     public ResponseEntity<TennisCourtDTO> findTennisCourtWithSchedulesById(Long tennisCourtId) {
         return ResponseEntity.ok(tennisCourtService.findTennisCourtWithSchedulesById(tennisCourtId));
     }
