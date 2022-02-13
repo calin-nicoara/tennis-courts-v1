5b2ef707591d67240fae59f91e3aa05f08e76ccd
calin.nicoara <calin.nicoara@esolutions.ro>
2022-02-13 11:14:01 +0200

    Pushed missing guests.sql script

---
+++ src/test/resources/sql/guests.sql
@@ -0,0 +1,5 @@
+DELETE FROM guest;
+
+insert into guest(id, name) values(1, 'Tom Hanks');
+insert into guest(id, name) values(2, 'Maria Sharapova');
+insert into guest(id, name) values(3, 'Maria Williams');
